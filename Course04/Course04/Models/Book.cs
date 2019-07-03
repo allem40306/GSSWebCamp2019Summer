654c1d338fbda9d8812fe8dd45b4635ab140e6e2
allem40306 <allem40306@gmail.com>
2019-07-03 09:33:20 +0800

    add data in Book Model

--- Course04/Course04/Models/Book.cs
+++ Course04/Course04/Models/Book.cs
@@ -10,29 +10,31 @@ namespace Course04.Models
     public class Book
     {
 
+        //[Required(ErrorMessage = "此欄位必填")]
+
         [DisplayName("書名")]
-        [Required(ErrorMessage = "此欄位必填")]
         public string BookName { get; set; }
 
         [DisplayName("作者")]
-        [Required(ErrorMessage = "此欄位必填")]
         public string BookAuthor { get; set; }
 
         [DisplayName("出版商")]
-        [Required(ErrorMessage = "此欄位必填")]
         public string BookPublisher { get; set; }
 
         [DisplayName("內容簡介")]
-        [Required(ErrorMessage = "此欄位必填")]
-        public string BookBriefIntroduction { get; set; }
+        public string BookNote { get; set; }
 
         [DisplayName("購書日期")]
-        [Required(ErrorMessage = "此欄位必填")]
         public string BuyDate { get; set; }
 
         [DisplayName("圖書類別")]
-        [Required(ErrorMessage = "此欄位必填")]
         public string BookClass { get; set; }
+
+        [DisplayName("借閱狀態")]
+        public string BookStatus { get; set; }
+
+        [DisplayName("借閱人")]
+        public string Keeper { get; set; }
 
 
     }
